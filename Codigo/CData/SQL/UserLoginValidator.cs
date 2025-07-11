9584bc99e1345306afa7d4593db7b522218dfe4e
Galo Izquierdo V <36722870+gizquier@users.noreply.github.com>
2025-07-10 21:53:52 -0500

    Add files via upload

---
+++ Codigo/CData/SQL/UserLoginValidator.cs
@@ -0,0 +1,34 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace CData.SQL
+{
+    public class UserLoginValidator
+    {
+        private readonly ConnectionDB connectionDB;
+
+        public UserLoginValidator()
+        {
+            connectionDB = new ConnectionDB();
+        }
+
+        public bool ValidarUsuario(string usuarioCorreo, string contrasena)
+        {
+            using (MySqlConnection conn = connectionDB.OpenConnection())
+            {
+                string query = @"SELECT COUNT(*) FROM users 
+                                 WHERE (usuario = @valor OR correo = @valor) 
+                                 AND clave = @clave";
+
+                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@valor", usuarioCorreo);
+                    cmd.Parameters.AddWithValue("@clave", contrasena);
+
+                    int count = Convert.ToInt32(cmd.ExecuteScalar());
+                    return count > 0;
+                }
+            }
+        }
+    }
+}
+
